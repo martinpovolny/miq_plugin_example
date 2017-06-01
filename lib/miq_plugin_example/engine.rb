76b1c249aa2b921e93b10d5e4feaaa156364c13b
Dávid Halász <dhalasz@redhat.com>
2017-06-01 10:09:41 +0200

    Pluggable menu item for the /plug page

--- lib/miq_plugin_example/engine.rb
+++ lib/miq_plugin_example/engine.rb
@@ -1,5 +1,13 @@
 module MiqPluginExample
   class Engine < ::Rails::Engine
-    #isolate_namespace MiqPluginExample
+    isolate_namespace MiqPluginExample
+
+    initializer 'plugin' do
+      Menu::CustomLoader.register(
+        Menu::Section.new(:spike, N_('Plugin'), 'fa fa-map-pin', [
+          Menu::Item.new('plug', N_('Test'), 'miq_report', {:feature => 'miq_report', :any => true}, '/plug')
+        ])
+      )
+    end
   end
 end
